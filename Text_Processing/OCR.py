7f4813e501f99f34a9c253f589ac2ff5c21e5b2a
Stalk4r <flakjacket06@protonmail.com>
2023-11-19 15:04:52 +0530

    Integrated OCR and regex

--- Text_Processing/OCR.py
+++ Text_Processing/OCR.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[343]:
-
-
 import cv2 as cv
 import numpy as np
-from PIL import Image
+import os
+from PIL import Image, ImageOps
 
 import pytesseract
 
-main_image='realtimeImage5.jpg'
+main_image='test-300.png'
 
 # Set the path to the Tesseract executable
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
@@ -19,27 +17,32 @@ def extractTextFromImage(image):
     custom_config = r'--oem 3 -l eng'
     extractedText = pytesseract.image_to_string(image,config=custom_config)
     return extractedText
+
 input_image_path = main_image
 extracted_text = extractTextFromImage(input_image_path)
 
 # Now you can work with the extracted text
 print(extracted_text)
 
+def compress_image(input_path, target_size):
+    image = Image.open(input_path)
+    image = ImageOps.exif_transpose(image)  # Correct the image orientation
+   
+    quality = 100
+    while os.path.exists(input_path) and os.path.getsize(input_path) > target_size:
+        print("Compressing Image")
+        print(os.path.getsize(input_path))
+        # Save the image with the current quality level
+        image.save(input_path, optimize=True, quality=quality)
 
-# In[344]:
-
-
-import pytesseract
-
-# Set the path to the Tesseract executable
-pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-
-
-# In[345]:
-
-
-import cv2 as cv
-from PIL import Image
+        # Break the loop if the quality level reaches 0
+        if quality <= 0:
+            break
+    # Optionally, you can reapply the EXIF transpose if needed
+    image = ImageOps.exif_transpose(image)
+    
+    # Return the compressed image
+    return image
 
 def preprocess_image(image_path, output_path, threshold_methods):
     img = cv.imread(image_path)
@@ -68,7 +71,7 @@ def preprocess_image(image_path, output_path, threshold_methods):
             compressed_image = compress_image(f"{method_name}_threshold.png", target_size=1024 * 1024)
 
             # Extract text from the compressed image
-            extracted_text = extract_text_from_image(compressed_image)
+            extracted_text = extractTextFromImage(compressed_image)
 
             # Compare the length of the extracted text
             if len(extracted_text) > best_length:
@@ -104,33 +107,7 @@ print(f"Best preprocessed image saved as {output_image_path}")
 print(f"Best thresholding method: {best_method}, Extracted Text: \n{best_text}")
 
 
-# In[347]:
-
-
-from PIL import Image, ImageOps
-import os
-
 image_path = r"best_preprocessed_image.png"
-
-def compress_image(input_path, target_size):
-    image = Image.open(input_path)
-    image = ImageOps.exif_transpose(image)  # Correct the image orientation
-   
-    quality = 100
-    while os.path.exists(input_path) and os.path.getsize(input_path) > target_size:
-        print("Compressing Image")
-        print(os.path.getsize(input_path))
-        # Save the image with the current quality level
-        image.save(input_path, optimize=True, quality=quality)
-
-        # Break the loop if the quality level reaches 0
-        if quality <= 0:
-            break
-    # Optionally, you can reapply the EXIF transpose if needed
-    image = ImageOps.exif_transpose(image)
-    
-    # Return the compressed image
-    return image
 
 # Define the target size you want and call the function with it
 target_size = 1024 * 1024  # For example, 1 MB
@@ -144,7 +121,7 @@ else:
     print(f"Error: Unable to load the image at '{image_path}'")
 
 
-# In[348]:
+
 
 
 def extractTextFromImage(image):
@@ -159,7 +136,7 @@ input_image_path = main_image
 print(extracted_text)
 
 
-# In[350]:
+
 
 
 # List of image paths
@@ -170,7 +147,7 @@ for image_path in image_paths:
 
     # Original image OCR
     original_image = cv.imread(image_path)
-    original_text = extract_text_from_image(original_image)
+    original_text = extractTextFromImage(original_image)
     print("Original Image OCR Result:")
     print(original_text)
 
@@ -187,20 +164,17 @@ for image_path in image_paths:
     print(f"\nChosen Source: {chosen_source}, Extracted Text: \n {chosen_text}")
 
 
-# In[351]:
-
-
 with open("OCR_TEXT.txt","w") as fp:
     fp.write(chosen_text)
 
 
-# In[ ]:
 
 
 
 
 
-# In[ ]:
+
+
 
 
 
--- Text_Processing/text_processing.py
+++ Text_Processing/text_processing.py
@@ -17,6 +17,7 @@ nutrition_pat={
     "Carbohydrate":r'(?<=Carbohydrate)[\s.,\(\)\|a-zA-Z]*(\d+.{0,1}\d*\w{0,2})',
     "Calcium":r'(?<=Calcium)[\s.,\(\)\|a-zA-Z]*(\d+.{0,1}\d*\w{0,2})',
     "Protein":r'(?<=Protein)[\s.,\(\)\|a-zA-Z]*(\d+.{0,1}\d*\w{0,2})',
+    "Sugars":r'(?<=Sugars)[\s.,\(\)\|a-zA-Z]*(\d+.{0,1}\d*\w{0,2})'
 }
 
 txt=open("OCR_TEXT.txt").read()
@@ -25,14 +26,19 @@ nutrient_out={}
 
 for pat in nutrition_pat:
 
-    tt=None
-    
+    tt=None   
     for i in txt.split("\n"):
+    
+        i=re.sub(r'\(.*\)','',i)
+        i=re.sub(r'[^\w.]',' ',i)
+
         pp=re.compile(nutrition_pat[pat])
         res=re.search(pp,i)
         if res:
             tt=res.group(1)
     
+    if tt:
+        tt=re.sub(r'[^\d.]','',tt)
     nutrient_out[pat]=tt
 
 
@@ -43,33 +49,50 @@ if nutrient_out["Energy"]:
 
 ##INGREDIENTS
 
-ingredients=re.sub(r'[\[\]\(\)]',',',txt)
+ingredients=re.sub(r'[^\w,. ]',' ',txt)
+ingredients=re.sub(r'[\d]+\.*[\d]*',',',ingredients)
 ingredients=re.sub(r'and',',',ingredients)
 ingredients=re.sub(r'from|From','',ingredients)
-#print(ingredients)
-ingredients_regex=r'(Ingredients|INGREDIENTS|ingredients):([\w\[\]\(\),\s]*)[\n]*'
-ingredients=re.search(ingredients_regex,ingredients).group(2)
+ingredients=re.sub(r'[ ]+',' ',ingredients)
 
-ingredients=ingredients.split(",")
+ingredients_regex=r'(Ingredients|INGREDIENTS|ingredients):*([\w\[\]\(\),\s]*)[\n]*'
+res=re.search(ingredients_regex,ingredients)
+ingredients=[]
 
-#remove invalid elements, strip whitespaces and auto correct
-ingredients=list(filter(lambda x:not re.search(r'\d',x),ingredients))
-ingredients=list(map(lambda x:re.sub(r'[\s+]',' ',x),ingredients))
-#ingredients=list(map(lambda x:re.sub(r' ','',x),ingredients))
+for i in list(range(1,3))[::-1]:
+    try:
+        ingredients=res.group(i)
+        break
+    except:
+        continue
 
-#spell check
+if ingredients:
+    ingredients=ingredients.split(",")
+    ingredients=list(map(lambda x:re.sub(r'[\s+]',' ',x),ingredients))
 
-#ingredients=list(map(lambda x:spell(x),ingredients))
-ingredients=list(filter(len,ingredients))
+    #spell check
+    #ingredients=list(map(lambda x:spell(x),ingredients))
+    ingredients=list(filter(len,ingredients))
+    ingredients=list(filter(lambda x:not re.match(r'[ ]+$',x),ingredients))
 
 #allergens part
 allergens=re.sub(r'[\[\]\(\)]',',',txt)
 allergens=re.sub(r'and',',',allergens)
 
 allergen_regex=r'(Contains|CONTAINS|May Contain|contain)(.*)'
-allergens=re.search(allergen_regex,allergens).group(2)
+res=re.search(allergen_regex,allergens)
+allergens=[]
 
-allergens=allergens.split(',')
+for i in range(1,3)[::-1]:
+    try:
+        allergens=res.group(i)    
+        break
+    except:
+        continue
+
+if allergens:
+    allergens=allergens.split(',')
+    allergens=list(map(lambda x:re.sub('[\W]','',x),allergens))
 
 out_obj={"Nutrients":nutrient_out,"Ingredients":ingredients,"Allergens":allergens}
 
